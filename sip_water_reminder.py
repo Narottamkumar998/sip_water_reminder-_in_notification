17275cc8bd1f8be7c3286bce1537a9d6762c88b7
Narottamkumar998 <narottamk998@gmail.com>
2025-08-01 13:57:40 +0530

    Add files via upload
    Build a Drink Water Reminder App in Python that notifies users to drink water at regular intervals. You can use libraries like time for timing and plyer for notifications. This app helps you practice basic logic, user interaction, and periodic reminders in Python.

---
+++ sip_water_reminder.py
@@ -0,0 +1,8 @@
+import time
+from plyer import notification
+
+while True:
+    print("please  drink some water !")
+    notification.notify(title="please drik some water",
+                        message=" you need to drink some water")
+    time.sleep( 60*60scsd)
